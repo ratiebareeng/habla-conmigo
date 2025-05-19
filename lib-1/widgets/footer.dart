b8fb822f5e9fd25d4294442fadc947ede0036bb1
ratiebareeng <bareengbiz@gmail.com>
2025-05-19 18:55:01 +0200

    Update Footer ui

--- lib-1/widgets/footer.dart
+++ lib-1/widgets/footer.dart
@@ -1,16 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../providers/theme_provider.dart';
 
 class Footer extends StatelessWidget {
+  const Footer({super.key});
+
   @override
   Widget build(BuildContext context) {
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDark = themeProvider.isDarkMode;
+
     return Container(
-      padding: EdgeInsets.all(16.0),
-      color: Colors.grey[200],
-      child: Center(
-        child: Text(
-          'Footer Content',
-          style: TextStyle(fontSize: 16.0, color: Colors.black54),
-        ),
+      padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 24),
+      color: isDark
+          ? const Color(0xFF1E293B) // slate-800
+          : Colors.white.withOpacity(0.8),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            '© 2025 HablaConmigo. All rights reserved.',
+            style: TextStyle(
+              fontSize: 12,
+              color: isDark ? Colors.grey[400] : Colors.grey[600],
+            ),
+          ),
+          Row(
+            children: [
+              Text(
+                'Made with ',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: isDark ? Colors.grey[400] : Colors.grey[600],
+                ),
+              ),
+              Icon(
+                Icons.favorite,
+                color: Colors.red[400],
+                size: 14,
+              ),
+              Text(
+                ' for language learners',
+                style: TextStyle(
+                  fontSize: 12,
+                  color: isDark ? Colors.grey[400] : Colors.grey[600],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
     );
   }
