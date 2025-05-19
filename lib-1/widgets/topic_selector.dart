d375aeb14d4f0c9776a55ef62b88ee9316524d89
ratiebareeng <bareengbiz@gmail.com>
2025-05-19 18:52:44 +0200

    Update TopicSelector ui

--- lib-1/widgets/topic_selector.dart
+++ lib-1/widgets/topic_selector.dart
@@ -1,37 +1,76 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class TopicSelector extends StatefulWidget {
-  final List<String> topics;
-  final ValueChanged<String> onTopicSelected;
+import '../models/chat.dart';
+import '../providers/theme_provider.dart';
 
-  TopicSelector({required this.topics, required this.onTopicSelected});
+class TopicSelector extends StatelessWidget {
+  final Topic selectedTopic;
+  final Function(Topic) onSelectTopic;
 
-  @override
-  _TopicSelectorState createState() => _TopicSelectorState();
-}
-
-class _TopicSelectorState extends State<TopicSelector> {
-  String? selectedTopic;
+  const TopicSelector({
+    super.key,
+    required this.selectedTopic,
+    required this.onSelectTopic,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return DropdownButton<String>(
-      value: selectedTopic,
-      hint: Text('Select a topic'),
-      items: widget.topics.map((String topic) {
-        return DropdownMenuItem<String>(
-          value: topic,
-          child: Text(topic),
-        );
-      }).toList(),
-      onChanged: (String? newValue) {
-        setState(() {
-          selectedTopic = newValue;
-        });
-        if (newValue != null) {
-          widget.onTopicSelected(newValue);
-        }
-      },
+    final themeProvider = Provider.of<ThemeProvider>(context);
+    final isDark = themeProvider.isDarkMode;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Conversation Topic',
+          style: TextStyle(
+            fontSize: 14,
+            fontWeight: FontWeight.w500,
+            color: isDark ? Colors.grey[300] : Colors.grey[700],
+          ),
+        ),
+        const SizedBox(height: 8),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            border: Border.all(
+              color: isDark ? Colors.grey[700]! : Colors.grey[300]!,
+            ),
+          ),
+          child: DropdownButtonHideUnderline(
+            child: DropdownButton<Topic>(
+              value: selectedTopic,
+              isExpanded: true,
+              padding: const EdgeInsets.symmetric(horizontal: 12),
+              borderRadius: BorderRadius.circular(8),
+              dropdownColor: isDark ? Colors.grey[800] : Colors.white,
+              icon: Icon(
+                Icons.arrow_drop_down,
+                color: isDark ? Colors.grey[400] : Colors.grey[700],
+              ),
+              items: TopicData.allTopics.map((TopicData topicData) {
+                return DropdownMenuItem<Topic>(
+                  value: topicData.value,
+                  child: Text(
+                    '${topicData.label} - ${topicData.description}',
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: isDark ? Colors.grey[300] : Colors.grey[800],
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                );
+              }).toList(),
+              onChanged: (Topic? value) {
+                if (value != null) {
+                  onSelectTopic(value);
+                }
+              },
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
