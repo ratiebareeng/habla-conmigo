2da4e73d0e36515bf9770c8652f643081d35e582
ratiebareeng <bareengbiz@gmail.com>
2025-05-20 17:17:26 +0200

    Implement VapiApiService

--- lib/services/vapi_api_service.dart
+++ lib/services/vapi_api_service.dart
@@ -1,3 +1,6 @@
+import 'dart:developer';
+
+import 'package:habla_conmigo/api_key.dart';
 import 'package:vapi/Vapi.dart';
 
 class VapiApiService {
@@ -5,9 +8,46 @@ class VapiApiService {
 
   static VapiApiService get instance => _instance;
 
-  var vapi = Vapi('your-public-key');
+  var vapi = Vapi(vapiPublicKey);
 
   factory VapiApiService() => _instance;
 
   VapiApiService._internal();
+
+  // listen to the Vapi call
+  Stream<VapiEvent> listen() {
+    return vapi.onEvent.map((event) {
+      if (event.label == "call-start") {
+        log('call started');
+      }
+      if (event.label == "call-end") {
+        log('call ended');
+      }
+
+      // Speech statuses, function calls and transcripts will be sent via messages
+      if (event.label == "message") {
+        log(event.value);
+      }
+      return event;
+    });
+  }
+
+  // Mute user mic
+  Future<void> muteMic(bool mute) async {
+    vapi.setMuted(mute);
+  }
+
+  // Start the Vapi call
+  Future<void> start({
+    String? assistantId,
+    dynamic assistant,
+    dynamic assistantOverrides = const {},
+  }) async {
+    await vapi.start(assistant: vapiAssistantRaul);
+  }
+
+  // Stop the Vapi call
+  Future<void> stop() async {
+    await vapi.stop();
+  }
 }
